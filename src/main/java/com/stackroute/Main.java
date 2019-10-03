f0b5d8a2402f585fa486d17c5679ad5f2cdc5ce7
sou.das <sou.das@cgi.com>
2019-10-03 14:53:25 +0530

    spring-xml-demo-autowire-xml

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,17 +1,14 @@
 package com.stackroute;
 
 
-import com.stackroute.demo.BeanLifecycleDemoBean;
 import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -19,19 +16,35 @@ import org.springframework.core.io.Resource;
 import java.beans.XMLDecoder;
 import java.io.FileNotFoundException;
 
-
 public class Main
 {
     public static void main( String[] args ) throws FileNotFoundException
     {
-       ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        /*without property*/
+        Movie movie = (Movie) applicationContext.getBean("movie1");
+        System.out.println(movie);
+        /* with  property byName*/
+        Movie movie1=(Movie)applicationContext.getBean("movie2");
+        System.out.println(movie1);
 
-        BeanLifecycleDemoBean   beanLifecycleDemoBean=(BeanLifecycleDemoBean)applicationContext.getBean("blcd1");
-        System.out.println(beanLifecycleDemoBean);
+        /* with constructor */
+        Movie movie2=(Movie)applicationContext.getBean("movie3");
+        System.out.println(movie2);
 
-        ClassPathXmlApplicationContext cxt=(ClassPathXmlApplicationContext)applicationContext;
-        cxt.close();
+        Movie movie4=(Movie)applicationContext.getBean("movie4");
+        System.out.println(movie4);
+
+        /* Exception while using byType*/
+
+       /*Exception in thread "main" org.springframework.beans.factory.UnsatisfiedDependencyException:
+        Error creating bean with name 'movie3' defined in class path resource
+        [beans.xml]: Unsatisfied dependency expressed through bean property 'actor';
+        nested exception is org.springframework.beans.factory.NoUniqueBeanDefinitionException:
+        No qualifying bean of type 'com.stackroute.domain.Actor' available:
+        expected single matching bean but found 3: actor*/
 
     }
+
 
 }

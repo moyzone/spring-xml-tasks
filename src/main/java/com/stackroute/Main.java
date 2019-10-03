c910ad3893084451e7fb51547de7fd95f4679a1d
sou.das <sou.das@cgi.com>
2019-10-03 12:24:45 +0530

    Initial Commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -12,7 +12,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-
 import java.beans.XMLDecoder;
 import java.io.FileNotFoundException;
 

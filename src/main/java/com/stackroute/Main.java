4b91f769a30910284a8a8b0c1d34529b49b242db
sou.das <sou.das@cgi.com>
2019-10-03 14:26:16 +0530

    spring-xml-demo-bean-post-process

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,14 +1,18 @@
 package com.stackroute;
 
 
+import com.stackroute.demo.BeanLifecycleDemoBean;
+import com.stackroute.demo.BeanPostProcessorDemoBean;
 import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -16,59 +20,22 @@ import org.springframework.core.io.Resource;
 import java.beans.XMLDecoder;
 import java.io.FileNotFoundException;
 
+
 public class Main
 {
     public static void main( String[] args ) throws FileNotFoundException
     {
-       /* System.out.println( "Hello World!" );
-        Actor  actor= new Actor("Salman","Male",29);
-        System.out.println(actor); */
+       ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
 
-        /* 1st method */
-       /* Resource resource= new ClassPathResource("beans.xml");
-        BeanFactory factory= new XmlBeanFactory(resource);
-        Actor actr1=(Actor) factory.getBean("actr1");
-        System.out.println(actr1); */
-/*        BeanFactory factory= new XmlBeanFactory(resource);
-        Movie movie3=(Movie) factory.getBean("movie1");
-        System.out.println(movie3);
+        BeanLifecycleDemoBean   beanLifecycleDemoBean=(BeanLifecycleDemoBean)applicationContext.getBean("blcd1");
+        System.out.println(beanLifecycleDemoBean);
 
-        System.out.println("2nd method"); */
+        BeanPostProcessorDemoBean beanPostProcessorDemoBean=(BeanPostProcessorDemoBean)applicationContext.getBean("bppdb1") ;
+        System.out.println(beanPostProcessorDemoBean);
 
-
-        /* 2nd method*/
-        /*
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Actor actr2 = (Actor) applicationContext.getBean("actr2");
-        System.out.println(actr2); */
-
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie = (Movie) applicationContext.getBean("movie1");
-        System.out.println(movie);
-
-        //applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2 = (Movie) applicationContext.getBean("movie1");
-        System.out.println(movie2);
-        System.out.println("Without scope property");
-        System.out.println((Movie) applicationContext.getBean("movie1")==(Movie) applicationContext.getBean("movie1"));
-        /* with scope prototype*/
-        System.out.println("With scope property");
-        System.out.println((Movie) applicationContext.getBean("movie2")==(Movie) applicationContext.getBean("movie2"));
-        /* use name */
-        Movie movie5=(Movie) applicationContext.getBean("movie3");
-        Movie movie6=(Movie) applicationContext.getBean("movie4");
-        System.out.println(movie5);
-        System.out.println(movie6);
-
-
-       /* BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        reader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie movie4= (Movie) ((DefaultListableBeanFactory) (beanDefinitionRegistry)).getBean("movie2");
-        System.out.println(movie4); */
-
+        ClassPathXmlApplicationContext cxt=(ClassPathXmlApplicationContext)applicationContext;
+        cxt.close();
 
     }
-
 
 }
